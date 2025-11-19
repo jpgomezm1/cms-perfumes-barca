fa9c34c649be06c868566e9a4cc3aceb1c3b2172
Juan Pablo Gomez <jpgomezm@eafit.edu.co>
2025-11-19 16:46:43 -0500

    Cambios Last

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      providerOptions: {
+        localServer: {
+          maxage: 300000
+        },
+      },
+      sizeLimit: 10 * 1024 * 1024, // 10MB
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
+      }
+    }
+  }
+});
